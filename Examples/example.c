5bc45648996389732f49086b205bf7110cbe5ce1
Ikenna Odukwe <ikenna@login-el8.khoury.northeastern.edu>
2025-01-12 21:19:53 -0500

    Update example.c with new modifications

--- Examples/example.c
+++ Examples/example.c
@@ -1,6 +1,16 @@
 #include <stdio.h> 
 
+//This is my the entry point to my program.
 int main(){
-	printf("Hello World!\n");
+	int accountID = 5;
+	double accountBalance = 100.53;
+	char accountType = 'c';
+	printf("AccountID is: %d\n", accountID);
+	printf("AccountBal is: %f\n", accountBalance);
+	printf("AccountTy is: %c\n", accountType);
+
+	printf("sizeof(int): %d\n", sizeof(int));
+	printf("sizeof(double): %d\n", sizeof(double));
+	printf("sizeof(char): %d\n", sizeof(char));
 	return 0;
 }
